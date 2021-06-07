637b2614fe4e541d9d65141fdf260fba8f397a77
jakobbraun <jakob.braun@posteo.de>
2021-06-07 15:02:17 +0200

    Refactor/15 remove testcontainer (#19)
    * #11: Removed exasol-testconatiner compile dependency

---
+++ src/test/java/com/exasol/udfdebugging/modules/TestSetup.java
@@ -0,0 +1,58 @@
+package com.exasol.udfdebugging.modules;
+
+import java.io.Closeable;
+import java.sql.*;
+import java.util.stream.Stream;
+
+import com.exasol.bucketfs.Bucket;
+import com.exasol.containers.ExasolContainer;
+import com.exasol.dbbuilder.dialects.exasol.*;
+import com.exasol.dbbuilder.dialects.exasol.udf.UdfScript;
+import com.exasol.udfdebugging.ExposedServiceAddress;
+import com.exasol.udfdebugging.LocalServiceExposer;
+
+/**
+ * This class contains common integration test setup.
+ */
+public class TestSetup implements Closeable, AutoCloseable {
+    private static final String SCHEMA_NAME = "TEST";
+    private static final String UDF_NAME = "HELLO_WORLD";
+
+    private final ExasolContainer<? extends ExasolContainer<?>> exasol = new ExasolContainer<>().withReuse(true);
+
+    public TestSetup() {
+        this.exasol.start();
+    }
+
+    public LocalServiceExposer getHostPortProxy() {
+        return port -> new ExposedServiceAddress(this.exasol.getHostIp(), port);
+    }
+
+    public Bucket getDefaultBucket() {
+        return this.exasol.getDefaultBucket();
+    }
+
+    public ExasolContainer<? extends ExasolContainer<?>> getExasolContainer() {
+        return this.exasol;
+    }
+
+    public void runJavaUdf(final Stream<String> jvmOptions) throws SQLException {
+        try (final Connection connection = this.exasol.createConnection();
+                final Statement statement = connection.createStatement()) {
+            final ExasolObjectFactory exasolObjectFactory = new ExasolObjectFactory(connection,
+                    ExasolObjectConfiguration.builder().withJvmOptions(jvmOptions.toArray(String[]::new)).build());
+            final ExasolSchema schema = exasolObjectFactory.createSchema(SCHEMA_NAME);
+            schema.createUdfBuilder(UDF_NAME).inputType(UdfScript.InputType.SCALAR).language(UdfScript.Language.JAVA)
+                    .content("class HELLO_WORLD {\n"
+                            + " static String run(ExaMetadata exa, ExaIterator ctx) throws Exception {\n"
+                            + " \treturn \"\";\n" + " }\n" + "}")
+                    .returns("VARCHAR(2000)").build();
+            statement.executeQuery("SELECT " + SCHEMA_NAME + "." + UDF_NAME + "()").close();
+        }
+    }
+
+    @Override
+    public void close() {
+        this.exasol.stop();
+    }
+}
--- src/test/java/com/exasol/udfdebugging/modules/coverage/CoverageModuleIT.java
+++ src/test/java/com/exasol/udfdebugging/modules/coverage/CoverageModuleIT.java
@@ -4,47 +4,24 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.*;
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.jacoco.core.data.ExecutionDataReader;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-import com.exasol.containers.ExasolContainer;
-import com.exasol.dbbuilder.dialects.exasol.*;
-import com.exasol.dbbuilder.dialects.exasol.udf.UdfScript;
 import com.exasol.errorreporting.ExaError;
-import com.exasol.udfdebugging.ExposedServiceAddress;
-import com.exasol.udfdebugging.LocalServiceExposer;
+import com.exasol.udfdebugging.modules.TestSetup;
 
-@Testcontainers
 class CoverageModuleIT {
-    @Container
-    private static final ExasolContainer<? extends ExasolContainer<?>> EXASOL = new ExasolContainer<>().withReuse(true);
-    private static final String SCHEMA_NAME = "TEST";
-    private static final String UDF_NAME = "HELLO_WORLD";
-
-    private static LocalServiceExposer getHostPortProxy() {
-        return port -> new ExposedServiceAddress(EXASOL.getHostIp(), port);
-    }
 
     @Test
-    void testCoverageReportIsWritten() throws SQLException, IOException, InterruptedException {
+    void testCoverageReportIsWritten() throws SQLException, IOException {
         deleteExecutionFile();
-        final Connection connection = EXASOL.createConnection();
-        final CoverageModule coverageModule = new CoverageModule(getHostPortProxy(), EXASOL.getDefaultBucket());
-        final ExasolObjectFactory exasolObjectFactory = new ExasolObjectFactory(connection, ExasolObjectConfiguration
-                .builder().withJvmOptions(coverageModule.getJvmOptions().toArray(String[]::new)).build());
-        final ExasolSchema schema = exasolObjectFactory.createSchema(SCHEMA_NAME);
-        schema.createUdfBuilder(UDF_NAME).inputType(UdfScript.InputType.SCALAR).language(UdfScript.Language.JAVA)
-                .content("class HELLO_WORLD {\n"
-                        + " static String run(ExaMetadata exa, ExaIterator ctx) throws Exception {\n"
-                        + " \treturn \"hello world\";\n" + " }\n" + "}")
-                .returns("VARCHAR(2000)").build();
-        connection.createStatement().executeQuery("SELECT " + SCHEMA_NAME + "." + UDF_NAME + "()");
+        final TestSetup udfSetup = new TestSetup();
+        final CoverageModule coverageModule = new CoverageModule(udfSetup.getHostPortProxy(),
+                udfSetup.getDefaultBucket());
+        udfSetup.runJavaUdf(coverageModule.getJvmOptions());
         assertThat(countReportedJacocoSessions(), equalTo(1));
     }
 
