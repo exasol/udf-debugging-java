d3c304628e566333f3213a5f60a14d3d482b7ffb
Christoph Pirkl <christoph.pirkl@exasol.com>
2024-03-01 09:48:58 +0100

    Refactor test

--- src/test/java/com/exasol/udfdebugging/modules/udflogs/LogRecorderTest.java
+++ src/test/java/com/exasol/udfdebugging/modules/udflogs/LogRecorderTest.java
@@ -1,6 +1,9 @@
 package com.exasol.udfdebugging.modules.udflogs;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.io.*;
 import java.net.Socket;
@@ -9,7 +12,6 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 class LogRecorderTest {
@@ -17,41 +19,37 @@ class LogRecorderTest {
     @Test
     void testLogsAreWrittenAsFile() throws Exception {
         final List<Path> logFiles = new ArrayList<>();
-        final LogRecorder logRecorder = new LogRecorder(logFiles::add);
-        final StreamToLogger connection = new StreamToLogger(logRecorder.getPort());
-        connection.write("test");
-        assertThat(logFiles, Matchers.hasSize(1));
-        assertThat(Files.readString(logFiles.get(0)), Matchers.equalTo("test"));
-        connection.close();
-        logRecorder.close();
+        try (final LogRecorder logRecorder = new LogRecorder(logFiles::add);
+                final StreamToLogger connection = new StreamToLogger(logRecorder.getPort());) {
+            connection.write("test");
+            assertAll(() -> assertThat(logFiles, hasSize(1)),
+                    () -> assertThat(Files.readString(logFiles.get(0)), equalTo("test")));
+        }
     }
 
     @Test
     void testParallelStreams() throws Exception {
         final List<Path> logFiles = new ArrayList<>();
-        final LogRecorder logRecorder = new LogRecorder(logFiles::add);
-        final StreamToLogger connection1 = new StreamToLogger(logRecorder.getPort());
-        connection1.write("test");
-        assertThat(logFiles, Matchers.hasSize(1));
-        assertThat(Files.readString(logFiles.get(0)), Matchers.equalTo("test"));
-        final StreamToLogger connection2 = new StreamToLogger(logRecorder.getPort());
-        connection2.write("other");
-        assertThat(logFiles, Matchers.hasSize(2));
-        assertThat(Files.readString(logFiles.get(1)), Matchers.equalTo("other"));
-        connection1.close();
-        connection2.close();
-        logRecorder.close();
+        try (final LogRecorder logRecorder = new LogRecorder(logFiles::add);
+                final StreamToLogger connection1 = new StreamToLogger(logRecorder.getPort());) {
+            connection1.write("test");
+            assertAll(() -> assertThat(logFiles, hasSize(1)),
+                    () -> assertThat(Files.readString(logFiles.get(0)), equalTo("test")));
+            try (final StreamToLogger connection2 = new StreamToLogger(logRecorder.getPort())) {
+                connection2.write("other");
+                assertAll(() -> assertThat(logFiles, hasSize(2)),
+                        () -> assertThat(Files.readString(logFiles.get(1)), equalTo("other")));
+            }
+        }
     }
 
     private static class StreamToLogger implements Closeable {
         private final Socket socket;
-        private final OutputStream outputStream;
         private final PrintWriter writer;
 
         public StreamToLogger(final int port) throws IOException {
             this.socket = new Socket("localhost", port);
-            this.outputStream = this.socket.getOutputStream();
-            this.writer = new PrintWriter(this.outputStream);
+            this.writer = new PrintWriter(this.socket.getOutputStream());
         }
 
         @SuppressWarnings("java:S2925") // sleep is required
@@ -64,7 +62,6 @@ class LogRecorderTest {
         @Override
         public void close() throws IOException {
             this.writer.close();
-            this.outputStream.close();
             this.socket.close();
         }
     }
