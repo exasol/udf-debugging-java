b23448a06ab76280d1a017e1f2c1794e962a194c
Christoph Pirkl <4711730+kaklakariada@users.noreply.github.com>
2025-06-02 12:02:55 +0200

    #74: Upgrade dependencies (#76)

--- src/test/java/com/exasol/udfdebugging/modules/coverage/CoverageModuleTest.java
+++ src/test/java/com/exasol/udfdebugging/modules/coverage/CoverageModuleTest.java
@@ -18,11 +18,14 @@ import com.exasol.bucketfs.BucketAccessException;
 class CoverageModuleTest {
 
     @Test
+    @SuppressWarnings("try") // auto-closeable resource coverageModule is never referenced in body of try statement
     void testUpload() throws BucketAccessException, TimeoutException, FileNotFoundException {
         final Bucket bucket = mock(Bucket.class);
-        new CoverageModule((port) -> new InetSocketAddress("1.2.3.4", port), bucket);
-        verify(bucket).uploadFile(Path.of("target", "jacoco-agent", "org.jacoco.agent-runtime.jar"),
-                "org.jacoco.agent-runtime.jar");
+        try (CoverageModule coverageModule = new CoverageModule(port -> new InetSocketAddress("1.2.3.4", port),
+                bucket)) {
+            verify(bucket).uploadFile(Path.of("target", "jacoco-agent", "org.jacoco.agent-runtime.jar"),
+                    "org.jacoco.agent-runtime.jar");
+        }
     }
 
     @Test
@@ -30,9 +33,10 @@ class CoverageModuleTest {
         final Bucket bucket = mock(Bucket.class);
         when(bucket.getBucketFsName()).thenReturn("my_bucketfs");
         when(bucket.getBucketName()).thenReturn("my_bucket");
-        final CoverageModule coverageModule = new CoverageModule((port) -> new InetSocketAddress("1.2.3.4", port),
-                bucket);
-        assertThat(coverageModule.getJvmOptions().collect(Collectors.toList()), contains(
-                "-javaagent:/buckets/my_bucketfs/my_bucket/org.jacoco.agent-runtime.jar=output=tcpclient,address=1.2.3.4,port=3002"));
+        try (final CoverageModule coverageModule = new CoverageModule(port -> new InetSocketAddress("1.2.3.4", port),
+                bucket)) {
+            assertThat(coverageModule.getJvmOptions().collect(Collectors.toList()), contains(
+                    "-javaagent:/buckets/my_bucketfs/my_bucket/org.jacoco.agent-runtime.jar=output=tcpclient,address=1.2.3.4,port=3002"));
+        }
     }
 }
